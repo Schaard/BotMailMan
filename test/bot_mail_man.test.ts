b67b186c2eb9f1520746e7c6080f5e612adbed5b
DESKTOP-DC5LUBI\Schaard <Schaard@gmail.com>
2024-09-23 21:39:01 -0700

    Initial commit

---
+++ test/bot_mail_man.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BotMailMan from '../lib/bot_mail_man-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bot_mail_man-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BotMailMan.BotMailManStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
